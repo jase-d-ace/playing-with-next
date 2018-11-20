3704ef761534e194293622fad6d38a8b748c87b7
Jason Andrada <jpa323@nyu.edu>
2018-11-20 15:43:56 -0500

    initial commit

---
+++ pages/about.js
@@ -0,0 +1,9 @@
+import Layout from './layout';
+
+const About = () => (
+  <Layout>
+    <p>Hello About!</p>
+  </Layout>
+);
+
+export default About
---
+++ pages/index.js
@@ -0,0 +1,10 @@
+import Link from 'next/link';
+import Layout from './layout';
+
+const Index = () => (
+  <Layout>
+    <p>Hello World!</p>
+  </Layout>
+);
+
+export default Index;
