b10466c74790e05bab212a5d1d1092b7d648a49f
Jason Andrada <jpa323@nyu.edu>
2018-11-20 19:07:25 -0500

    hard code initial posts example

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,22 @@
 import Link from 'next/link';
 import Layout from './layout';
 
-const Index = () => (
-  <Layout>
-    <p>Hello World!</p>
-  </Layout>
+
+const PostLink = props => (
+  <li>
+    <Link href={`/post?title=${props.title}`}>
+      <a>{props.title}</a>
+    </Link>
+  </li>
 );
 
-export default Index;
+export default () => (
+  <Layout>
+    <h1>My Blog!</h1>
+    <ul>
+      <PostLink title="Hard Coded Title" />
+      <PostLink title="Hard Coded Title 2: Electric Boogaloo" />
+      <PostLink title="Hard Coded Title 3: The Most Hard Coded" />
+    </ul>
+  </Layout>
+);
